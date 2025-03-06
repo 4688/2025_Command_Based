018ff9a962e5924b13e9a512bcacd68934508d8a
Saints Bot #4688 <46527170+saintsbot@users.noreply.github.com>
2025-03-05 21:59:02 -0500

    //swevre fixs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,7 +45,7 @@ public class RobotContainer
                                                                 () -> driverXbox.getLeftX() * -1)
                                                             .withControllerRotationAxis(driverXbox::getRightX)
                                                             .deadband(OperatorConstants.DEADBAND)
-                                                            .scaleTranslation(0.8)
+                                                            .scaleTranslation(1)
                                                             .allianceRelativeControl(true);
 
   /**
